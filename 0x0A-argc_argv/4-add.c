3a3171674dbee4dfae154aad83f425b65c401eb9
benlaguroun <benlaguroun@gmail.com>
2023-06-14 15:52:32 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <string.h>
 /**
-* check_num - check - string there are digit
-* @str: array str
-* Return: Always 0 (Success)
-*/
-int main(int argc, char *argv[]) 
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
 {
-int sum = 0;
-for (int i = 1; i < argc; i++) 
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) 
+
+	{
+		if (!isdigit(str[count])) 
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+int main(int argc, char *argv[])
+
 {
-for (int j = 0; argv[i][j] != '\0'; j++) 
-{
-if (!isdigit(argv[i][j])) 
-{
-printf("Error\n");
-return 1;
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) 
+	{
+		if (check_num(argv[count]))
+
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
-}
-int num = atoi(argv[i]);
-if (num > 0) 
-{
-sum += num;
-}
-}
-printf("%d\n", sum);
-return 0;
-}
+

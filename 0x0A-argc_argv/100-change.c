9268b5f690ebd8049f8336fcd0cbb1e4d2ba4583
benlaguroun <benlaguroun@gmail.com>
2023-06-14 15:44:32 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main-prints the minimum coins
- * make change for amont money
- * @argc:number of commandline
- * @argv:pointer to an array of command line
- * return:0 (success).
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
+int main(int argc, char *argv[])
+{
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-int main(int argc, char *argv[]) 
-{
-if (argc != 2) 
-{
-printf("Error\n");
-return 1;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
 }
- int cents = atoi(argv[1]);
- int coins[] = {25, 10, 5, 2, 1};
- int numCoins = sizeof(coins) / sizeof(coins[0]);
- int minCoins = 0;
- for (int i = 0; i < numCoins; i++) 
-{
-minCoins += cents / coins[i];
-cents %= coins[i];
-}
-printf("%d\n", (cents < 0 ? 0 : minCoins));
-return 0;
-}
+

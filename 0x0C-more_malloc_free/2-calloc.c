3f990a06814f56743b302f2dcdc9f506da8fdd5d
benlaguroun <benlaguroun@gmail.com>
2023-06-16 04:41:27 -0700

    22

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,16 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-for (unsigned int i = 0; i < n; i++) 
-{
-*(s + i) = b;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
-return (s);
-}
+
 
 /**
  * *_calloc - allocates memory for an array, usng malloc

2c3166a931f8e14fcc9b18e5244505e506d57ffd
benlaguroun <benlaguroun@gmail.com>
2023-07-10 11:42:05 +0100

    22

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,26 +9,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-		return (-1);
-	int result = -1;  
-	FILE *file = fopen(filename, "a");  
-	if (file != NULL)
-	{
-		if (text_content != NULL)
-		{
-			int len = 0;
-			while (text_content[len])
-				len++;
-			size_t bytes_written = fwrite(text_content, sizeof(char), len, file);
-			if (bytes_written == (size_t)len)
-				result = 1;  
-		}
-		else
-		{
-			result = 1;  
-		}
-		fclose(file);  
-	}
-	return (result);
+if (filename == NULL)
+return (-1);
+FILE *file = fopen(filename, "a");
+if (file == NULL)
+return (-1);
+if (text_content != NULL) 
+{
+if (fputs(text_content, file) == EOF) 
+{
+fclose(file);
+return (-1);
+}
+}
+fclose(file);
+return (1);
 }

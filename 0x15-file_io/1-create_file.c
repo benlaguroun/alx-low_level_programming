43da2bbe153f04aa3dba5ae60da1610f089a67b7
benlaguroun <benlaguroun@gmail.com>
2023-07-10 11:24:36 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,52 +1,30 @@
 #include "main.h"
 
 /**
- * get_text_length - Calculates the length of a string.
- * @str: The input string.
- *
- * Return: The length of the string.
- */
-size_t get_text_length(const char *str)
-{
-	size_t length = 0;
-
-	if (str != NULL)
-	{
-		while (str[length])
-			length++;
-	}
-
-	return (length);
-}
-
-/**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @filename: name of file to create.
+ * @text_content: text to write
  *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Return: 1 on success 0 on failure
  */
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int fd, w;
-	size_t len = get_text_length(text_content);
-
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
-	w = write(fd, text_content, len);
-	if (w == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
-
-	return (1);
+int fd, w, len = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len]; len++);
+}
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+w = write(fd, text_content, len);
+if (w == -1)
+{
+close(fd);
+return (-1);
+}
+close(fd);
+return (1);
 }

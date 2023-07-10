51de09b56955b64b0d770c3217a35b3b353aa493
benlaguroun <benlaguroun@gmail.com>
2023-07-10 11:12:15 +0100

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * get_length - Calculates the length of a string.
+ * @str: A pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+size_t get_length(const char *str)
+{
+	size_t length = 0;
+
+	while (str[length])
+		length++;
+
+	return (length);
+}
+
+/**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
@@ -10,16 +26,14 @@
  */
 int create_file(const char *filename, const char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, w;
+	size_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
-	}
+		len = get_length(text_content);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
